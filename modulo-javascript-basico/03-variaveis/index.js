6470294d450115eac2b3ef813c3abfed40109da5
Guilherme Souza <guilhermesouzadossantos11@gmail.com>
2023-03-03 06:51:12 -0300

    iniciando aulas de JS

---
+++ modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,21 @@
+var nomePessoa = "Guilherme";
+
+const emailPessoa = "gui@gmail.com";
+
+let telefonePessoa = "11966638879"
+
+
+// As variáveis no javascript, possuem escopos, cada escopo possui seu metodo de utilização;
+// Esses escopos possuem nomes e suas caracteristicas, escopo global, função e bloco.
+
+// Escopo Global, seria o escopo onde a variával pode ser utilizada em qualquer momento ou parte do código.
+
+// Escopo de Função, seria a variavel que pode ser usada somente dentro de uma função.
+
+// Escopo de Bloco, pode ser usada somente dentro de chaves, if, else, laços de repetição.
+
+{
+    console.log(nomePessoa);
+    console.log(emailPessoa);
+    console.log(telefonePessoa);
+}
